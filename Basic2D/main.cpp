f19fdb4e119dcab5fca9b5e4254bee995ffb1166
Chln94 <ch_leoni@yahoo.it>
2018-12-06 13:32:06 +0100

    Update main.cpp

--- Basic2D/main.cpp
+++ Basic2D/main.cpp
@@ -50,6 +50,7 @@ std::vector<char> answer;
 POINTS last_mouse_p;
 LRESULT	CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);	// Declaration For WndProc
 // ---------------------------------------------------------
+//vtvfytgv
 ///////////////////////////////////////////////////////////
 //  kill the window
 GLvoid KillGLWindow(GLvoid)								// Properly Kill The Window
